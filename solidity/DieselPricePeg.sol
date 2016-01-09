63db90d4f9acb7d4db9fffea90ba7dc09841b903
Thomas Bertani <mail@thomasbertani.it>
2016-01-09 09:10:03 +0000

    new DieselPricePeg example

---
+++ solidity/DieselPricePeg.sol
@@ -0,0 +1,26 @@
+import "dev.oraclize.it/api.sol";
+
+contract DieselPricePeg is usingOraclize {
+    
+    uint public DieselPriceUSD;
+    
+
+    function DieselPricePeg() {
+        oraclize_setNetwork(networkID_testnet);
+        oraclize_setProof(proofType_TLSNotary | proofStorage_IPFS);
+        update(0); // first check at contract creation
+    }
+
+    function __callback(bytes32 myid, string result, bytes proof) {
+        if (msg.sender != oraclize_cbAddress()) throw;
+        DieselPriceUSD = parseInt(result, 2); // let's save it as $ cents
+        // do something with the USD Diesel price
+        update(60*10); // schedule another check in 10 minutes
+    }
+    
+    function update(uint delay) {
+        oraclize_query(delay, "URL", "xml(https://www.fueleconomy.gov/ws/rest/fuelprices).fuelPrices.diesel");
+    }
+    
+}
+
