f38111c27118c338d8a5164b061107e713ec82f4
Thomas Bertani <mail@thomasbertani.it>
2017-05-18 15:44:15 +0100

    Update randomExample.sol

--- solidity/random-datasource/randomExample.sol
+++ solidity/random-datasource/randomExample.sol
@@ -33,7 +33,7 @@ contract RandomExample is usingOraclize {
         uint N = 7; // number of random bytes we want the datasource to return
         uint delay = 0; // number of seconds to wait before the execution takes place
         uint callbackGas = 200000; // amount of gas we want Oraclize to set for the callback function
-        bytes32 queryId = oraclize_newRandomDSQuery(delay, nbytes, callbackGas); // this function internally generates the correct oraclize_query and returns its queryId
+        bytes32 queryId = oraclize_newRandomDSQuery(delay, N, callbackGas); // this function internally generates the correct oraclize_query and returns its queryId
     }
     
 }
