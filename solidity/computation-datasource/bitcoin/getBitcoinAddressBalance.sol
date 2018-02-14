ab7c7fb65196088f31dcf84c7a0c4ba4883fa0ab
Marco Giglio <marco@oraclize.it>
2018-02-14 11:06:18 +0000

    Rename folders

---
+++ solidity/computation-datasource/bitcoin/getBitcoinAddressBalance.sol
@@ -0,0 +1,31 @@
+/*
+   Oraclize Bitcoin Example
+*/
+
+
+pragma solidity ^0.4.0;
+import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
+
+contract BitcoinAddressExample is usingOraclize {
+    
+    // Address balance in Satoshis
+    uint256 balance;
+
+    event BitcoinAddressBalance(uint _balance);
+    
+    function BitcoinAddressExample() {
+        oraclize_setProof(proofType_TLSNotary | proofStorage_IPFS); 
+        getBalance("3D2oetdNuZUqQHPJmcMDDHYoqkyNVsFk9r");
+    }
+
+    function __callback(bytes32 myid, string result, bytes proof) {
+        if (msg.sender != oraclize_cbAddress()) throw;
+        balance = parseInt(result, 8);
+        BitcoinAddressBalance(balance);
+    }
+    
+    function getBalance(string _bitcoinAddress) payable {
+        oraclize_query("computation",["QmaMFiHXSqCFKkGPbWZh5zKmM827GWNpk9Y1EYhoLfwdHq", _bitcoinAddress]);
+    }
+    
+}
