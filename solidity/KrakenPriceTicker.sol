5317a01f676808c4e2de256ad7be3492c49eaf31
~Luiserebii~ <43155488+Luiserebii@users.noreply.github.com>
2018-10-22 14:45:11 -0400

    Updating KrakenPriceTicker.sol

--- solidity/KrakenPriceTicker.sol
+++ solidity/KrakenPriceTicker.sol
@@ -4,36 +4,36 @@
    This contract keeps in storage an updated ETH/XBT price,
    which is updated every ~60 seconds.
 */
-pragma solidity ^0.4.0;
-
-import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
-
+pragma solidity ^0.4.25;
+import "https://raw.githubusercontent.com/oraclize/ethereum-api/master/oraclizeAPI_0.4.25.sol";
 
 contract KrakenPriceTicker is usingOraclize {
 
     string public priceETHXBT;
 
-    event NewOraclizeQuery(string description);
-    event NewKrakenPriceTicker(string price);
+    event NewOraclizeQuery(string _description);
+    event NewKrakenPriceTicker(string _price);
 
-    function KrakenPriceTicker() public {
+    constructor() public payable {
         oraclize_setProof(proofType_Android | proofStorage_IPFS);
         update();
     }
 
-    function __callback(bytes32 myid, string result, bytes proof) public {
+    function __callback(bytes32 _myid, string _result, bytes _proof) public {
         if (msg.sender != oraclize_cbAddress()) revert();
-        priceETHXBT = result;
-        NewKrakenPriceTicker(priceETHXBT);
+        priceETHXBT = _result;
+        emit NewKrakenPriceTicker(priceETHXBT);
         update();
     }
 
     function update() public payable {
         if (oraclize_getPrice("URL") > this.balance) {
-            NewOraclizeQuery("Oraclize query was NOT sent, please add some ETH to cover for the query fee");
+            emit NewOraclizeQuery("Oraclize query was NOT sent, please add some ETH to cover for the query fee");
         } else {
-            NewOraclizeQuery("Oraclize query was sent, standing by for the answer..");
+            emit NewOraclizeQuery("Oraclize query was sent, standing by for the answer...");
             oraclize_query(60, "URL", "json(https://api.kraken.com/0/public/Ticker?pair=ETHXBT).result.XETHXXBT.c.0");
         }
     }
 }
+
+
