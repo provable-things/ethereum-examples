0f4fce070d2d6d937de0a1cde21c3f7618042637
~Luiserebii~ <43155488+Luiserebii@users.noreply.github.com>
2018-10-22 14:51:22 -0400

    Updating YoutubeViews.sol

--- solidity/YoutubeViews.sol
+++ solidity/YoutubeViews.sol
@@ -1,35 +1,33 @@
 /*
-    Youtube video views
+    Youtube Video Views
 
     This contract keeps in storage a views counter
     for a given Youtube video.
 */
 
-pragma solidity ^0.4.0;
-
-import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
-
+pragma solidity ^0.4.25;
+import "https://raw.githubusercontent.com/oraclize/ethereum-api/master/oraclizeAPI_0.4.25.sol";
 
 contract YoutubeViews is usingOraclize {
 
     string public viewsCount;
 
-    event NewOraclizeQuery(string description);
-    event NewYoutubeViewsCount(string views);
+    event NewOraclizeQuery(string _description);
+    event NewYoutubeViewsCount(string _views);
 
-    function YoutubeViews() public {
+    constructor() public payable {
         update();
     }
 
-    function __callback(bytes32 myid, string result) public {
+    function __callback(bytes32 _myid, string _result) public {
         if (msg.sender != oraclize_cbAddress()) revert();
-        viewsCount = result;
-        NewYoutubeViewsCount(viewsCount);
-        // do something with viewsCount. like tipping the author if viewsCount > X?
+        viewsCount = _result;
+        emit NewYoutubeViewsCount(viewsCount);
+        // Do something with viewsCount. Like tipping the author if viewsCount > x?
     }
 
     function update() public payable {
-        NewOraclizeQuery("Oraclize query was sent, standing by for the answer..");
+        emit NewOraclizeQuery("Oraclize query was sent, standing by for the answer...");
         oraclize_query("URL", 'html(https://www.youtube.com/watch?v=9bZkp7q19f0).xpath(//*[contains(@class, "watch-view-count")]/text())');
     }
 }
