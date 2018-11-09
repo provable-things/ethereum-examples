e8e001c3d4e176467beeb8aeb2145c71797687e1
Greg Kapka <greg@oraclize.it>
2018-11-09 20:17:53 +0000

    add streamr tweets truffle example

---
+++ solidity/truffle-examples/streamr/README.md
@@ -0,0 +1,45 @@
+# :wrench: :construction: Testing Oraclize's Streamr Tweets Example.
+
+&nbsp;
+
+This repo is to demonstrate how you would set up an Oraclize smart-contract development environment using Truffle & the Ethereum-Bridge to do most of the heavy lifting for you. Head on over to the `./test` folder to examine the javascript files that thoroughly test the smart-contract, which latter you will find in `./contracts`.
+
+## :page_with_curl:  _Instructions_
+
+**1):** Fire up your favourite console & make sure you have Truffle 5 installed:
+
+__`❍ npm install -g truffle@beta`__
+
+**2)** Clone this repo somewhere:
+
+__`❍ git clone https://github.com/oraclize/ethereum-examples.git`__
+
+**3)** Enter this directory & install dependencies:
+
+__`❍ cd ethereum-examples/solidity/truffle-examples/streamr && npm install`__
+
+**4)** Launch Truffle:
+
+__`❍ truffle develop`__
+
+**5)** Open a _new_ console in the same directory & spool up the ethereum-bridge:
+
+__`❍ ./node_modules/.bin/ethereum-bridge -a 9 -H 127.0.0.1 -p 9545 --dev`__
+
+**6)** Once the bridge is ready & listening, go back to the first console with Truffle running & set the tests going!
+
+__`❍ test`__
+
+&nbsp;
+
+## :camera: Passing Tests:
+
+[The passing tests!](streamr-tests.jpg)
+
+&nbsp;
+
+## :black_nib: Notes:
+
+__❍__ If you have any issues, head on over to our [Gitter](https://gitter.im/oraclize/ethereum-api?raw=true) channel to get timely support!
+
+__*Happy developing!*__
---
+++ solidity/truffle-examples/streamr/test/streamr-tweets-counter-test.js
@@ -0,0 +1,37 @@
+const Web3 = require('web3')
+const {waitForEvent} = require('./utils')
+const streamrTweetsCounter = artifacts.require('./StreamrTweetsCounter.sol')
+const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:9545'))
+
+contract('Streamr Tweets Counter Example Tests', accounts => {
+
+  let numTweets
+  const gasAmt = 3e6
+  const address = accounts[0]
+
+  beforeEach(async () => (
+    {contract} = await streamrTweetsCounter.deployed(), 
+    {methods, events} = new web3.eth.Contract(contract._jsonInterface, contract._address) 
+  ))
+
+  it('Should have logged a new Oraclize query', async () => {
+    const {returnValues:{description}} = await waitForEvent(events.LogNewOraclizeQuery) 
+    assert.equal(description, 'Oraclize query was sent, standing by for the answer...', 'Oraclize query incorrectly logged!') 
+  })
+
+  it('Should retrieve number of tweets over the last minute', async () => {
+    const {returnValues:{result}} = await waitForEvent(events.LogResult)
+    numTweets = result
+    assert.isAbove(parseInt(result), 0, 'No result was retrieved from the offchain twitter stream!')
+  })
+  
+  it('Should have saved the number of tweets in the smart-contract', async () => {
+    const tweetCount = await methods.numberOfTweets().call()
+    assert.equal(parseInt(tweetCount), parseInt(numTweets), 'Number of tweets should have been saved after Oraclize callback')
+  })
+
+  it('Should log a failed second query due to lack of funds', async () => {
+    const {events:{LogNewOraclizeQuery:{returnValues:{description}}}} = await methods.update().send({from: address, gas: gasAmt})
+    assert.equal(description, 'Oraclize query was NOT sent, please add some ETH to cover for the query fee', 'Oraclize query incorrectly logged!')
+  })
+})
