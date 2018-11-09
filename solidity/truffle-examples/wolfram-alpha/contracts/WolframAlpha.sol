06dfe2943d829cbd9e9d98e813f5433264abcde3
Greg Kapka <greg@oraclize.it>
2018-11-09 16:09:53 +0000

    add wolfram-alpha truffle example

---
+++ solidity/truffle-examples/wolfram-alpha/contracts/WolframAlpha.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.25;
+
+import "./oraclizeAPI.sol";
+
+contract WolframAlpha is usingOraclize {
+
+    string public temperature;
+
+    event LogNewOraclizeQuery(string description);
+    event LogNewTemperatureMeasure(string temperature);
+
+    constructor() public {
+        update(); // Update on contract creation...
+    }
+
+    function __callback(bytes32 myid, string result) public {
+        require(msg.sender == oraclize_cbAddress());
+        temperature = result;
+        emit LogNewTemperatureMeasure(temperature);
+        // Do something with the temperature measure...
+    }
+
+    function update() public payable {
+        emit LogNewOraclizeQuery("Oraclize query was sent, standing by for the answer...");
+        oraclize_query("WolframAlpha", "temperature in London");
+    }
+}
