ebd479c051d6cd97e1e0a9223be3732350f6e075
Greg Kapka <greg@oraclize.it>
2018-11-09 15:32:23 +0000

    add diesel prices truffle example

---
+++ solidity/truffle-examples/diesel-price/contracts/DieselPrice.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.25;
+
+import "./oraclizeAPI.sol";
+
+contract DieselPrice is usingOraclize {
+
+    uint public dieselPriceUSD;
+
+    event LogNewDieselPrice(string price);
+    event LogNewOraclizeQuery(string description);
+
+    constructor() public {
+        update(); // First check at contract creation...
+    }
+
+    function __callback(bytes32 myid, string result) public {
+        require(msg.sender == oraclize_cbAddress());
+        emit LogNewDieselPrice(result);
+        dieselPriceUSD = parseInt(result, 2); // Let's save it as cents...
+        // Now do something with the USD Diesel price...
+    }
+
+    function update() public payable {
+        emit LogNewOraclizeQuery("Oraclize query was sent, standing by for the answer...");
+        oraclize_query("URL", "xml(https://www.fueleconomy.gov/ws/rest/fuelprices).fuelPrices.diesel");
+    }
+}
