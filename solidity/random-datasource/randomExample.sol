47f524262b03fa688695f6d627631aa722120848
Riccardo Persiani <s225289@studenti.polito.it>
2018-11-09 11:15:49 +0000

    correct wrong range in some comments

--- solidity/random-datasource/randomExample.sol
+++ solidity/random-datasource/randomExample.sol
@@ -34,8 +34,8 @@ contract RandomExample is usingOraclize {
             newRandomNumber_bytes(bytes(_result)); // this is the resulting random number (bytes)
             
             // for simplicity of use, let's also convert the random bytes to uint if we need
-            uint maxRange = 2**(8* 7); // this is the highest uint we want to get. It should never be greater than 2^(8*N), where N is the number of random bytes we had asked the datasource to return
-            uint randomNumber = uint(sha3(_result)) % maxRange; // this is an efficient way to get the uint out in the [0, maxRange] range
+            uint maxRange = 2**(8* 7); // [maxRange - 1] is the highest uint we want to get.The variable maxRange should never be greater than 2^(8*N), where N is the number of random bytes we had asked the datasource to return
+            uint randomNumber = uint(sha3(_result)) % maxRange; // this is an efficient way to get the uint out in the [0, maxRange-1] range
             
             newRandomNumber_uint(randomNumber); // this is the resulting random number (uint)
         }
