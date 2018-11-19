11674d7f5dde165bbd3f33dc490096e4c4e79db0
Greg Kapka <greg@oraclize.it>
2018-11-19 16:12:41 +0000

    update bitcoin truffle example to solc & API 0.5.0

--- solidity/truffle-examples/bitcoin-balance/contracts/BitcoinAddressExample.sol
+++ solidity/truffle-examples/bitcoin-balance/contracts/BitcoinAddressExample.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.5.0;
 
 import "./oraclizeAPI.sol";
 
@@ -8,17 +8,17 @@ contract BitcoinBalanceExample is usingOraclize {
 
     event LogBitcoinAddressBalance(uint _balance);
     
-    constructor() {
+    constructor() public {
         getBalance("3D2oetdNuZUqQHPJmcMDDHYoqkyNVsFk9r");
     }
 
-    function __callback(bytes32 myid, string result, bytes proof) public {
+    function __callback(bytes32 myid, string memory result) public {
         require(msg.sender == oraclize_cbAddress());
         balance = parseInt(result, 8);
         emit LogBitcoinAddressBalance(balance);
     }
     
-    function getBalance(string _bitcoinAddress) public payable {
+    function getBalance(string memory _bitcoinAddress) public payable {
         oraclize_query("computation",["QmaMFiHXSqCFKkGPbWZh5zKmM827GWNpk9Y1EYhoLfwdHq", _bitcoinAddress]);
     }
 }
