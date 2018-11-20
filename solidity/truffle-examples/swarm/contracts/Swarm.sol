ecfb991ecd8ae10c269a448237e3b342ae35662f
Greg Kapka <greg@oraclize.it>
2018-11-20 08:56:34 +0000

    update swarm truffle example to use solc & oraclize API 0.5.0

--- solidity/truffle-examples/swarm/contracts/Swarm.sol
+++ solidity/truffle-examples/swarm/contracts/Swarm.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.5.0;
 
 import "./oraclizeAPI.sol";
 
@@ -9,18 +9,18 @@ contract Swarm is usingOraclize {
     event LogNewOraclizeQuery(string description);
     event LogNewSwarmContent(string swarmContent);
 
-    constructor() {
+    constructor() public {
         update(); // Update price on contract creation...
     }
     
-    function __callback(bytes32 myid, string result) {
+    function __callback(bytes32 myid, string memory result) public {
         require(msg.sender == oraclize_cbAddress());
         swarmContent = result;
         emit LogNewSwarmContent(result);
         // Do something with the swarm content...
     }
     
-    function update() payable {
+    function update() public payable {
         emit LogNewOraclizeQuery("Oraclize query was sent, standing by for the answer...");
         oraclize_query("swarm", "1dad37bcc272aa31d45128992be575820bececb13dd42c4cc87e4b6269067464");
     }
