1b09b88e0f071056867ba780ad07416cf73acfd0
Greg Kapka <greg@oraclize.it>
2019-03-21 09:30:44 +0000

    limit solidity pragma of delegated math example

--- solidity/computation-datasource/delegated-math/DelegatedMath.sol
+++ solidity/computation-datasource/delegated-math/DelegatedMath.sol
@@ -1,13 +1,13 @@
-pragma solidity ^0.5.0;
+pragma solidity >= 0.5.0 < 0.6.0;
 
 import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
 
 contract DelegatedMath is usingOraclize {
-    
+
     event LogOperationResult(uint result);
-    
+
     constructor() public {
-        oraclize_setProof(proofType_TLSNotary | proofStorage_IPFS); 
+        oraclize_setProof(proofType_TLSNotary | proofStorage_IPFS);
         delegateOperation("32", "125");
     }
 
@@ -15,7 +15,7 @@ contract DelegatedMath is usingOraclize {
         require(msg.sender == oraclize_cbAddress());
         emit LogOperationResult(parseInt(result));
     }
-    
+
     function delegateOperation(string memory firstOperand, string memory secondOperand) public payable {
         oraclize_query("computation",["Qmc8jmuT47cPWadF8ZhErGXj7J4VEp5H29knukCGirsN19", firstOperand, secondOperand]);
     }
