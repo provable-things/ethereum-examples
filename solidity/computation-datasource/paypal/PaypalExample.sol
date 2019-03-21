b76ebc7c1720d64e0035ec915431f75b67b6f8e8
Greg Kapka <greg@oraclize.it>
2019-03-21 09:31:11 +0000

    limit solidity pragma of paypal example

--- solidity/computation-datasource/paypal/PaypalExample.sol
+++ solidity/computation-datasource/paypal/PaypalExample.sol
@@ -1,13 +1,13 @@
-pragma solidity ^0.5.0;
+pragma solidity >= 0.5.0 < 0.6.0;
 
 import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
 
 contract PaypalExample is usingOraclize {
 
     event LogPaymentRe(string payment_info);
-    
+
     constructor() public {
-        oraclize_setProof(proofType_TLSNotary | proofStorage_IPFS); 
+        oraclize_setProof(proofType_TLSNotary | proofStorage_IPFS);
         createPayment("3","5");
     }
 
@@ -15,13 +15,13 @@ contract PaypalExample is usingOraclize {
         require(msg.sender == oraclize_cbAddress());
         emit LogPaymentResult(result);
     }
-    
+
     function createPayment(string memory _unitPrice, string memory _numberUnits) public payable {
     // This example will require a newly generate hookb.in, the following
 	// is just a placeholder
 	string memory ipFetcher = "https://hookb.in/Zm8d62bn";
         oraclize_query("computation",["QmZsVU2hsHYoETeKijxRq798qVm5RwbwoVNt2HcpTSgunn", _unitPrice,_numberUnits, "USD", ipFetcher]);
     }
-    
+
 }
 
