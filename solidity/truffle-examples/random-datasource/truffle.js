88ed8d3a9fa1098386d64e32ad0b0a91ff5b7b89
Riccardo Persiani <r.persiani92@gmail.com>
2019-03-22 11:16:35 +0000

    fix(truffle): .env support and minor improvments

--- solidity/truffle-examples/random-datasource/truffle.js
+++ solidity/truffle-examples/random-datasource/truffle.js
@@ -1,15 +1,18 @@
 const fs = require('fs')
 const path = require('path')
-const API_PATH = path.join(__dirname, 'apikeys.js')
-const HDWalletProvider = require("truffle-hdwallet-provider");
+const API_PATH = path.resolve(__dirname, 'apikeys.js')
+const HDWalletProvider = require("truffle-hdwallet-provider")
+const { fileExists } = require('./test/utils.js')
+require('dotenv').config()
 
-const fileExists = _path => fs.existsSync(_path)
 
-const getExternalVariable = _variable => fileExists(API_PATH)
-  ? require(API_PATH)[_variable]
-  : process.env[_variable]
-  ? process.env[_variable]
-  : (console.log(`Cannot migrate! Please provide '${_variable}' as an environment variable, or export it from '${API_PATH}'!`), process.exit(1))
+console.log(process.env)
+const getExternalVariable = _variable =>
+  fileExists(API_PATH)
+    ? require(API_PATH)[_variable]
+    : process.env[_variable]
+    ? process.env[_variable]
+    : (console.log(`Cannot migrate! Please provide '${_variable}' as an environment variable, or export it from '${API_PATH}'!`), process.exit(1))
 
 module.exports = {
   networks: {
@@ -30,7 +33,7 @@ module.exports = {
       provider: () => new HDWalletProvider(getExternalVariable('mnemonic'), `https://rinkeby.infura.io/v3/${getExternalVariable('infuraKey')}`),
       network_id: 4,
       gas: 6e6,
-      gasPrice: 5e9,	
+      gasPrice: 5e9,
       websockets: true
     },
     kovan: {
