fbc1da3ac99342ed61ad1dfb99c9b702dcb38b2c
D-Nice <d-nice2@protonmail.com>
2019-07-15 17:31:14 -0400

    fix(random): github -> local import

--- solidity/truffle-examples/random-datasource/contracts/RandomExample.sol
+++ solidity/truffle-examples/random-datasource/contracts/RandomExample.sol
@@ -10,7 +10,7 @@
  */
 pragma solidity >= 0.5.0 < 0.6.0;
 
-import "github.com/provable-things/ethereum-api/provableAPI.sol";
+import "./provableAPI.sol";
 
 contract RandomExample is usingProvable {
 
